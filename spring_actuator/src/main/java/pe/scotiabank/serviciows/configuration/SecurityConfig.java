049cf01b95667a1add9a86ca74605ea0f27e68e7
ebarrientos1979 <edwin.barrientos@iteraprocess.com>
2024-07-05 18:55:06 -0500

    Adecuacion Actuator

--- spring_actuator/src/main/java/pe/scotiabank/serviciows/configuration/SecurityConfig.java
+++ spring_actuator/src/main/java/pe/scotiabank/serviciows/configuration/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(request -> request.requestMatchers("/api/v1/auth/**")
+                .authorizeHttpRequests(request -> request.requestMatchers("/api/v1/auth/**", "/actuator/**")
                         .permitAll().anyRequest().authenticated())
                 .sessionManagement(manager -> manager.sessionCreationPolicy(STATELESS))
                 .authenticationProvider(authenticationProvider()).addFilterBefore(
